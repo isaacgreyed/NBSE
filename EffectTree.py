14a92c57004c2c2e0bf937d41f645af38835b165
Isaac Edelhauser <isaacgreyed@gmail.com>
2021-03-16 16:45:30 -0400

    NodeTree.py

--- EffectTree.py
+++ EffectTree.py
@@ -28,26 +28,3 @@ class EffectNode:
 class EffectTree:
     def __init__(self, root):
         self.root = root
-
-
-###example and testing
-
-
-def useless(): #placeholder function for testing
-    print("useless called")
-
-def passthrough(array):
-    return [array]
-
-def source():
-    return "source"
-
-passthrough_effect = EffectNode(passthrough, 1, 1, [], extra_args=["3213"])
-passthrough_effect.apply()
-
-print_effect = EffectNode(print, 1, 0, [(passthrough_effect, 1)])
-
-print_effect.apply()
-
-root = EffectNode(useless, 0, 0, [], [])
-root.apply()
