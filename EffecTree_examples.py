7ba60bc626aecebc917195405eb6511f20f035e1
Isaac Edelhauser <isaacgreyed@gmail.com>
2021-03-18 14:01:46 -0400

    simplified effect node code so everything is part of the tree

--- EffecTree_examples.py
+++ EffecTree_examples.py
@@ -10,12 +10,11 @@ def passthrough(array):
 def source():
     return "source"
 
-passthrough_effect = EffectNode(passthrough, 1, 1, [], [], extra_args=["3213"])
-passthrough_effect.apply()
+basic_param = EffectParam("01110")
 
-print_effect = EffectNode(print, 1, 0, [(passthrough_effect, 1)], [])
+passthrough_effect = EffectNode(passthrough, 1, 1, [(basic_param, 0)], [])
+#passthrough_effect.apply()
+
+print_effect = EffectNode(print, 1, 0, [(passthrough_effect, 0)], [])
 
 print_effect.apply()
-
-root = EffectNode(useless, 0, 0, [], [])
-root.apply()
--- EffectTree.py
+++ EffectTree.py
@@ -3,31 +3,36 @@ class Audio:
         self.data = data
 
 class EffectNode:
-    def __init__(self, function, input_amt: int, output_amt: int, inputs, outputs, extra_args=[]):
+    def __init__(self, function, input_amt: int, output_amt: int, inputs, outputs):
         self.function   = function   #function to be called to apply effect, should always return audio
         
         self.input_amt  = input_amt  #number of inputs function requires
         self.output_amt = output_amt #number of outputs function returns
         
-        self.inputs     = inputs     #where inputs to pass to function come from, think of lower nodes lower in the graph. Should be [(EffectNode, int)] and tie directly to inputs
-        self.outputs    = outputs    #where function outputs go think of farther along in the graph. Should be [(EffectNode, int)] and tie directly to inputs
+        self.inputs     = inputs#[None]*input_amt  #where inputs to pass to function come from, think of lower nodes lower in the graph. Should be [(EffectNode, int)] and tie directly to outputs
+        self.outputs    = []#[None]*output_amt #where function outputs go think of farther along in the graph. Should be [(EffectNode, int)] and tie directly to inputs
         
-        self.extra_args = extra_args #custom arguements to pass when applying
-        self.values     = []         #actually values of output function
+        self.values     = []         #actuall values of output function
 
     def apply(self) -> list: #call function, should apply effect to audio, args are optional additional arguements
         args = []
         for (effect, n) in self.inputs:
             args.append(effect.get(n))
-        args.append(self.extra_args)
         self.values = self.function(*args)
 
-    def get(self, value_num): #value_num, what output to get, if it returned two seperate vales 
+    def get(self, value_num): #value_num, what output to get. Start counting at 0
         self.apply()
-        if len(self.values) < value_num:
+        if len(self.values) < value_num+1:
             return None
         else:
-            return self.values[value_num-1]
+            return self.values[value_num]
+
+class EffectParam: #class to pass values into EffectNode class without other effect nodes
+    def __init__(self, value):
+        self.value = value
+
+    def get(self, value_num):
+        return self.value
 
 class EffectTree:
     def __init__(self, root):
