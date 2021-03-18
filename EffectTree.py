f6e2850a772049422885b1655c7ce209f3de2b41
Isaac Edelhauser <isaacgreyed@gmail.com>
2021-03-18 14:35:14 -0400

    simplified effect nodes removing inputs and outputs parameters and adding an add_input function.\nArray of None now fills inputs and outputs at correct length, based on input_amt and output_amt, by default

--- EffectTree.py
+++ EffectTree.py
@@ -3,35 +3,40 @@ class Audio:
         self.data = data
 
 class EffectNode:
-    def __init__(self, function, input_amt: int, output_amt: int, inputs, outputs):
+    def __init__(self, function, input_amt: int, output_amt: int):
         self.function   = function   #function to be called to apply effect, should always return audio
         
         self.input_amt  = input_amt  #number of inputs function requires
         self.output_amt = output_amt #number of outputs function returns
         
-        self.inputs     = inputs#[None]*input_amt  #where inputs to pass to function come from, think of lower nodes lower in the graph. Should be [(EffectNode, int)] and tie directly to outputs
-        self.outputs    = []#[None]*output_amt #where function outputs go think of farther along in the graph. Should be [(EffectNode, int)] and tie directly to inputs
-        
-        self.values     = []         #actuall values of output function
+        self.inputs     = [None]*input_amt  #where inputs to pass to function come from, think of lower nodes lower in the graph. Should be [(EffectNode, int)] and tie directly to outputs
+        self.outputs    = [None]*output_amt #where function outputs go think of farther along in the graph. Should be [(EffectNode, int)] and tie directly to inputs
 
-    def apply(self) -> list: #call function, should apply effect to audio, args are optional additional arguements
+    def apply(self) -> list: #call function, should apply effect to audio
         args = []
         for (effect, n) in self.inputs:
             args.append(effect.get(n))
-        self.values = self.function(*args)
+        return self.function(*args)
 
     def get(self, value_num): #value_num, what output to get. Start counting at 0
-        self.apply()
-        if len(self.values) < value_num+1:
+        vals = self.apply()
+        if len(vals) < value_num+1:
             return None
         else:
-            return self.values[value_num]
+            return vals[value_num]
+
+    #adds input to effect node
+    #argument_num is what position the argument should be in starting at 0
+    #effect is the EffectNode or EffectParam to get the effect from
+    #from_output_num tells us what number output we are getting, defaults to none for when it does not apply like in an EffectParam
+    def add_input(self, argument_num, effect, from_output_num=None):
+        self.inputs[argument_num] = (effect, from_output_num)
 
 class EffectParam: #class to pass values into EffectNode class without other effect nodes
     def __init__(self, value):
         self.value = value
 
-    def get(self, value_num):
+    def get(self, value_num=None): #value_num is a placeholder value to allow calling from EffectNodes
         return self.value
 
 class EffectTree:
