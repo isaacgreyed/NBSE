0f3222c8ff912bdbd96f0ed7f09ecb294746ff8e
Isaac Edelhauser <isaacgreyed@gmail.com>
2021-04-02 16:12:14 -0400

    main.py

--- effectFunctions.py
+++ effectFunctions.py
@@ -2,34 +2,19 @@ from pyo import *
 import os
 
 
-def add_reverb(byteArray, fre, qu, types):
-    with open('tmp.wav', 'wb') as tmpfile:
-        tmpfile.write(byteArray)
-
-
-    tmpfile = r'tmp.wav'
+def add_reverb(fre, qu, types):
+    tmpfile = "tmp.wav"
 
     s = Server(audio="offline").boot()
     filedur = sndinfo(tmpfile)[1]
 
-    s.recordOptions(dur=filedur, filename='tmpout.wav')
+    s.recordOptions(dur=filedur, filename=tmpfile)
     ifile = SfPlayer(tmpfile)
 
     filter = Biquad(ifile, freq=fre, q=qu, type=types).out()
     s.start()
 
-    with open('tmp.wav', 'rb') as originalF:
-        with open('tmpout.wav', 'rb') as newF:
-            newFile= newF.read()
-            backpointer = originalF.read()
-
-            originalF.close()
-            newF.close()
-
     s.stop()
-
-    #os.remove('tmpout.wav')
-    return newFile, backpointer
 
 
 def add_distortion(byteArray, slo, mult):
